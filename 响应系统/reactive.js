b1590cb6699bc8e09e155963e567c2f3b9e586d5
张东 <A80784@internet.com>
2023-09-08 10:25:07 +0800

    添加effect函数

--- 响应系统/reactive.js
+++ 响应系统/reactive.js
@@ -1,23 +1,43 @@
-//现在我们需要在触发get时收集副作用函数，触发set时执行与key相关的所有副作用函数
-//要在副作用函数与被操作的目标字段之间建立明确的联系，必须好好想想怎么设计存储副作用函数的数据结构
-//其中 WeakMap 的键是原始对象 target，WeakMap 的值是一个 Map 实例，而 Map 的键是原始对象 target 的 key，Map 的值是一个由副作用函数组成的 Set。
-const bucket = new WeakMap(); //bucket (weakMap+map+set)
-const data = { text: "hello", ok: true, num: 1 }; //data(被代理对象)
+let activeEffect; //用一个全局变量存储被注册的副作用函数
+const bucket = new WeakMap();
+const data = { text: "hello", ok: true, num: 1 };
+//先将副作用函数 effect 添加到桶里，即bucket.add(effect)，然后返回属性值
+//当设置属性值时先更新原始数据，再将副作用函数从桶里取出并重新执行
 const proxyData = new Proxy(data, {
-  //proxyData(代理对象)
   get(target, key) {
     console.log("get");
-    return target[key]; //监听到对proxyData(代理对象)get操作返回data(被代理对象)的值
+    //添加副作用函数过程
+    if (!activeEffect) return target[key];
+    let depsMap = bucket.get(target);
+    if (!depsMap) {
+      bucket.set(target, (depsMap = new Map()));
+    }
+    let deps = depsMap.get(key);
+    if (!deps) {
+      depsMap.set(key, (deps = new Set()));
+    }
+    deps.add(activeEffect);
+    return target[key];
   },
   set(target, key, newVal) {
     console.log("set");
-    target[key] = newVal; //监听到对proxyData(代理对象)set操作修改data(被代理对象)
+    target[key] = newVal;
+    //执行副作用函数过程
+    const depsMap = bucket.get(target);
+    if (!depsMap) return;
+    const effects = depsMap.get(key);
+    effects &&
+      effects.forEach((fn) => {
+        fn();
+      });
   },
 });
-proxyData.num++; //会触发get,set
-
-//map与weakMap的区别
-//简单地说，WeakMap对 key 是弱引用，不影响垃圾回收器的工作。据这个特性可知，一旦 key 被垃圾回收器回收，那么对应的键和值就访问不到了。
-//所以 WeakMap 经常用于存储那些只有当 key所引用的对象存在时（没有被回收）才有价值的信息，例如上面的场景中，如果 target 对象没有任何引用了，
-//说明用户侧不再需要它了， 这时垃圾回收器会完成回收任务。但如果使用 Map 来代替 WeakMap， 那么即使用户侧的代码对 target 没有任何引用，
-//这个 target 也不会被回收，最终可能导致内存溢出。
+//effect用来注册副作用函数的函数
+function effect(fn) {
+  activeEffect = fn;
+  fn();
+}
+effect(() => {
+  console.log(proxyData.text);
+});
+proxyData.text='changed'
