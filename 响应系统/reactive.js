71cee58ebe77da7271a6c4dae1024097f16f27f1
张东 <A80784@internet.com>
2023-09-08 11:07:13 +0800

    解决分支切换的问题

--- 响应系统/reactive.js
+++ 响应系统/reactive.js
@@ -1,8 +1,6 @@
-let activeEffect; //用一个全局变量存储被注册的副作用函数
+let activeEffect;
 const bucket = new WeakMap();
 const data = { text: "hello", ok: true, num: 1 };
-//先将副作用函数 effect 添加到桶里，即bucket.add(effect)，然后返回属性值
-//当设置属性值时先更新原始数据，再将副作用函数从桶里取出并重新执行
 const proxyData = new Proxy(data, {
   get(target, key) {
     console.log("get");
@@ -17,6 +15,8 @@ const proxyData = new Proxy(data, {
       depsMap.set(key, (deps = new Set()));
     }
     deps.add(activeEffect);
+    //收集 effectFn.deps 数组中的依赖集合 数组里装的是[set set set ...]
+    activeEffect.deps.push(deps)
     return target[key];
   },
   set(target, key, newVal) {
@@ -32,12 +32,34 @@ const proxyData = new Proxy(data, {
       });
   },
 });
-//effect用来注册副作用函数的函数
+//要将一个副作用函数从所有与之关联的依赖集合中移除，就需要
+// 明确知道哪些依赖集合中包含它，因此我们需要重新设计副作用函
+// 数，如下面的代码所示。在 effect 内部我们定义了新的 effectFn
+// 函数，并为其添加了 effectFn.deps 属性，该属性是一个数组，用
+// 来存储所有包含当前副作用函数的依赖集合
 function effect(fn) {
-  activeEffect = fn;
-  fn();
+  const effectFn = () => {
+// cleanup 函数接收副作用函数作为参数，遍历副作用函数的
+// effectFn.deps 数组，该数组的每一项都是一个依赖集合，然后将
+// 该副作用函数从依赖集合中移除，最后重置 effectFn.deps 数组。
+    cleanup(effectFn)
+    activeEffect = effectFn;
+    fn();
+  };
+  effectFn.deps = [];
+  effectFn();
+}
+function cleanup(effectFn) { 
+  for (let i = 0; i < effectFn.deps.length; i++) { 
+    effectFn.deps[i].delete(effectFn)
+    // 最后需要重置 effectFn.deps 数组
+    effectFn.deps.length = 0
+  }
 }
 effect(() => {
-  console.log(proxyData.text);
+  //分支切换产生的问题，ok为true时，text上有这个副作用函数，修改ok为false，text上不应有这个副作用函数，但是修改text的值还是会触发这个副作用
+  console.log(proxyData.ok ? proxyData.text : "-");
 });
-proxyData.text='changed'
+proxyData.ok = false;
+proxyData.text = "changed";
+//解决方法：在副作用执行之前，将其从相关联的依赖集合中移除，当副作用函数执行完毕后，会重新建立联系，但在新的联系中不会包含遗留的副作用函数
