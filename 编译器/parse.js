b3611c9ef052e74924c1813a8d8c69c72d437b4c
张东 <A80784@internet.com>
2023-10-23 14:45:39 +0800

    添加文本模式，处理缺少闭合标签的情况

--- 编译器/parse.js
+++ 编译器/parse.js
@@ -1,57 +1,69 @@
 function parse(content) {
-  const context = { source: content };
+  const context = { source: content, mode: "DATA" };
   return {
     type: "Root",
     children: parseChild(context, []),
   };
 }
 function isEnd(context, ancestors) {
-    const s = context.source;
-    if (ancestors.length !== 0) {
-        const tag = ancestors[ancestors.length - 1]?.tag;
-        if (tag === s.slice(2, 2 + tag.length)) return true;
-    }
+  const s = context.source;
+  for (let i = 0; i < ancestors.length; i++) {
+    
+    const tag = ancestors[ancestors.length - 1]?.tag;
+    if (tag === s.slice(2, 2 + tag.length))
+      return true;
+  }
   return !s;
 }
 function parseChild(context, ancestors) {
   const nodes = [];
-  while (!isEnd(context,ancestors)) {
+  //while 循环一定要遇到父级节点的结束标签才会停止
+  while (!isEnd(context, ancestors)) {
     let node;
-    if (context.source[0] === "<") {
-      if (/[a-z]/i.test(context.source[1])) {
-        node = parseElement(context, ancestors);
-      } else if (context.source[1] === "/") {
-          parseTag(context, "End");
+    // 只有 DATA 模式和 RCDATA 模式才支持插值节点的解析
+    if (context.mode === "DATA" || context.mode === "RCDATA") {
+      // 只有 DATA 模式才支持标签节点的解析
+      if (context.mode === "DATA" && context.source[0] === "<") {
+        if (/[a-z]/i.test(context.source[1])) {
+          node = parseElement(context, ancestors);
+        } else if (context.source[1] === "/") {
+          parseTag(context,'End')
+       continue
+        }
+      } else if (context.source.startsWith("{{")) {
+        node = parseInterpolation(context);
       }
-    } else if (context.source.startsWith("{{")) {
-      node = parseInterpolation(context);
-    } else {
+    }
+    // node 不存在，说明处于其他模式，即非 DATA 模式且非 RCDATA 模式
+    // 这时一切内容都作为文本处理
+    if (!node) {
+      // 解析文本节点
       node = parseText(context);
-      }
-      node&&nodes.push(node);
+    }
+
+    // 将节点添加到 nodes 数组中
+    nodes.push(node);
   }
   return nodes;
 }
 function parseInterpolation(context) {
-    const index = context.source.indexOf("}}");
-    const content = context.source.slice(2, index);
-    advanceBy(context,index+2)
-    return {
-        type: 'Interpolation',
-        content
+  const index = context.source.indexOf("}}");
+  const content = context.source.slice(2, index);
+  advanceBy(context,index + 2);
+  return {
+    type: "Interpolation",
+    content,
   };
 }
 function parseText(context) {
-    const indexI = context.source.indexOf("{{");
-    const indexE = context.source.indexOf("<");
-    const index=Math.min(indexI,indexE)
+  const indexI = context.source.indexOf("{{");
+  const indexE = context.source.indexOf("<");
+  const index = Math.min(indexI, indexE);
   const content = context.source.slice(0, index);
-  console.log(content)
-  advanceBy(context, content.length)
-  console.log(context.source)
-    return {
-        type: 'Text',
-        content
+  advanceBy(context, content.length);
+  return {
+    type: "Text",
+    content,
   };
 }
 function parseElement(context, ancestors) {
@@ -59,12 +71,18 @@ function parseElement(context, ancestors) {
   ancestors.push(element);
   element.children = parseChild(context, ancestors);
   ancestors.pop();
+  if (context.source.startsWith(`</${element.tag}`)) {
+    parseTag(context, "End");
+  } else {
+    // 缺少闭合标签
+    console.error(`${element.tag} 标签缺少闭合标签`);
+  }
   return element;
 }
 function parseTag(context, type) {
   const match = /^<\/?([a-z][^\t\r\n\f />]*)/i.exec(context.source);
   const tag = match[1];
-    advanceBy(context, type === "Start" ? 2 + tag.length : 3 + tag.length);
+  advanceBy(context, type === "Start" ? 2 + tag.length : 3 + tag.length);
   return type === "Start"
     ? {
         type: "Element",
@@ -75,5 +93,6 @@ function parseTag(context, type) {
 function advanceBy(context, nums) {
   context.source = context.source.slice(nums);
 }
-const content = "<div><p></p><span></span>Hi,{{message}}</div>";
-console.log(parse(content))
+const content = "<div><p>{{hi}}<span></div>";
+const a = parse(content);
+console.log(a);
